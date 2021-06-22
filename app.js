e4f1f4c0a1b559e1f4b0adf41849a25697062790
DouglasVDM <douglasvdmerwe@gmail.com>
2021-06-22 21:15:03 +0200

    custom events on my own objects

--- app.js
+++ app.js
@@ -1,9 +1,46 @@
 const events = require("events");
+const util = require("util");
 
-const myEmitter = new events.EventEmitter;
+// object constructor
+// const teams = function (name) {
+//   this.name = name;
+// }
 
-myEmitter.on("test", (arg) => {
-  console.log(arg);
+// util.inherits(teams, events.EventEmitter);
+
+// const Arsenal = new teams("Arsenal");
+// const Juventus = new teams("Juventus");
+// const Galatasaray = new teams("Galatasaray");
+
+// const teamArray = [Arsenal, Juventus, Galatasaray];
+
+// teamArray.forEach((team) => {
+//   team.on("nation", function (nation) {
+//     console.log(team.name + " is " + nation + "football club");
+//   });
+// });
+
+// Arsenal.emit("nation", "English");
+
+
+const countries = function(name) {
+  this.name = name;
+};
+
+util.inherits(countries, events.EventEmitter);
+
+const SouthAfrica = new countries("SouthAfrica");
+const Mauritius = new countries("Mauritius");
+const Ghana = new countries("Ghana");
+
+const countryArray = [SouthAfrica, Mauritius, Ghana];
+
+countryArray.forEach((country) => {
+  country.on("nation", (nation) => {
+    console.log(country.name + " is " + nation + " continent")
+  });
 });
 
-myEmitter.emit("test", "hello world");
+SouthAfrica.emit("nation", "African");
+Mauritius.emit("nation", "Tropical");
+Ghana.emit("nation", "Central African");
