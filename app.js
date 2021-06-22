e83fc72be4c6fb948b72c48e823e6dad3476acf7
DouglasVDM <douglasvdmerwe@gmail.com>
2021-06-22 21:40:39 +0200

    reading a file
    file will be read before the program moves on (synchronous).

--- app.js
+++ app.js
@@ -1,26 +1,5 @@
-const events = require("events");
-const util = require("util");
+const fs = require("fs");
 
-// object constructor
+const text = fs.readFileSync("read-me.txt", "utf-8");
 
-const countries = function(name) {
-  this.name = name;
-};
-
-util.inherits(countries, events.EventEmitter);
-
-const SouthAfrica = new countries("SouthAfrica");
-const Mauritius = new countries("Mauritius");
-const Ghana = new countries("Ghana");
-
-const countryArray = [SouthAfrica, Mauritius, Ghana];
-
-countryArray.forEach((country) => {
-  country.on("nation", (nation) => {
-    console.log(country.name + " is " + nation + " continent")
-  });
-});
-
-SouthAfrica.emit("nation", "African");
-Mauritius.emit("nation", "Tropical");
-Ghana.emit("nation", "Central African");
+console.log(text);
