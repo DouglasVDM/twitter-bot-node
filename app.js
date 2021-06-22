2c34f95ffdf9ff64a1e6e125a5d96be390b5bf07
DouglasVDM <douglasvdmerwe@gmail.com>
2021-06-22 22:31:00 +0200

    make directory with file inside it

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
 const fs = require("fs");
 
-fs.rmdirSync("my-folder");
+fs.mkdir("my-folder", () => {
+  fs.readFile("read-me.txt", "utf-8", (error, data) => {
+    fs.writeFile("./my-folder/write-me.txt", data, () => {      
+    })
+  })
+});
+
+// fs.unlink("./my-folder/write-me.txt", () => {  
+//   fs.rmdir("my-folder", (error) => {
+//     console.log(error);
+//   })
+// })
