44d854b71db875e9900bbdf377a368b8155d8deb
DouglasVDM <douglasvdmerwe@gmail.com>
2021-06-22 22:06:53 +0200

    write file asynchronous
    does not block code below. i don't have to wait for each request to be carried out first. makes it a lot quicker.

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const fs = require("fs");
 
 // this does not block the code of the application
 fs.readFile("read-me.txt", "utf-8", function (error, data) {
-  console.log(data);
-  console.log(error);
+  fs.writeFile("write.text", data, function (error) {
+    if(error) {
+      console.log(error)
+    }
+  })
 });
-
-// this code still fires while waiting on the file to read
-console.log("hello there");
