30501509a0aa1c791d2dc10b9b139b8f1a25b827
DouglasVDM <douglasvdmerwe@gmail.com>
2021-06-22 14:33:52 +0200

    global method 3

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 // console.log("hello world")
 
-setTimeout(() => {
-  console.log("hi there")
-}, 3000);
+// setTimeout(() => {
+//   console.log("hi there");
+// }, 3000);
+
+let count = 0;
+
+const timer = setInterval(() => {
+  count = count + 2
+  console.log(`${count} seconds have passed`);
+  if (count > 6) {
+    clearInterval(timer)
+  }
+}, 2000);
