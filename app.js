d1ea8d4087c3b4ab59d8980ce2bf0f0e514dd3df
DouglasVDM <douglasvdmerwe@gmail.com>
2021-06-22 22:12:24 +0200

    delete a file

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const fs = require("fs");
 
 // this does not block the code of the application
-fs.readFile("read-me.txt", "utf-8", function (error, data) {
-  fs.writeFile("write.text", data, function (error) {
-    if(error) {
-      console.log(error)
-    }
-  })
-});
+// fs.readFile("read-me.txt", "utf-8", function (error, data) {
+//   fs.writeFile("write.text", data, function (error) {
+//     if(error) {
+//       console.log(error)
+//     }
+//   })
+// });
+    
+fs.unlink("write.text", (error) => {
+  console.log(error);
+})
