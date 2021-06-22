b82dc6f206e900b20ca8751f0e15f889fce1cd76
DouglasVDM <douglasvdmerwe@gmail.com>
2021-06-22 19:43:59 +0200

    export multiple things from module

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
 const helper = require("./helper");
 
-console.log(helper("Alex"));
+console.log(helper.user("Alex"));
+console.log(helper.id("123"));
+console.log(helper.email("alex@gmail.co"));
--- helper.js
+++ helper.js
@@ -1,5 +1,15 @@
-const helper = (data) => {
+const user = (data) => {
   return `${data} is logged in`;
 };
 
-module.exports = helper
+let id = (id) => {
+  return `${id}`;
+};
+
+let email = (email) => {
+  return email;
+}
+
+module.exports.user = user;
+module.exports.id = id;
+module.exports.email = email;
