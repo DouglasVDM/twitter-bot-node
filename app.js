62dd9a3496f00bdd45ca6200b33e2a645bacbdc9
DouglasVDM <douglasvdmerwe@gmail.com>
2021-06-22 22:33:12 +0200

    remove directory with file inside
    1st remove file inside the directory
    2nd remove the directory
    *cannot remove directory that is not empty

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const fs = require("fs");
 
-fs.mkdir("my-folder", () => {
-  fs.readFile("read-me.txt", "utf-8", (error, data) => {
-    fs.writeFile("./my-folder/write-me.txt", data, () => {      
-    })
-  })
-});
-
-// fs.unlink("./my-folder/write-me.txt", () => {  
-//   fs.rmdir("my-folder", (error) => {
-//     console.log(error);
+// fs.mkdir("my-folder", () => {
+//   fs.readFile("read-me.txt", "utf-8", (error, data) => {
+//     fs.writeFile("./my-folder/write-me.txt", data, () => {      
+//     })
 //   })
-// })
+// });
+
+fs.unlink("./my-folder/write-me.txt", () => {  
+  fs.rmdir("my-folder", (error) => {
+    console.log(error);
+  })
+})
