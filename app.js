f7f9fa45aa1d1928053e2e36decf93568780047d
DouglasVDM <douglasvdmerwe@gmail.com>
2021-06-22 19:29:09 +0200

    create and require a module in another js file

--- app.js
+++ app.js
@@ -1,34 +1,3 @@
-// console.log("hello world")
+const helper = require("./helper");
 
-// setTimeout(() => {
-//   console.log("hi there");
-// }, 3000);
-
-// let count = 0;
-
-// const timer = setInterval(() => {
-//   count = count + 2
-//   console.log(`${count} seconds have passed`);
-//   if (count > 6) {
-//     clearInterval(timer)
-//   }
-// }, 2000);
-
-// console.log(__dirname)
-// console.log(__filename)
-
-// function blue() {
-//   console.log("This is a function.");
-// }
-
-// blue();
-
-function callbackFunction(func) {
-  func();
-}
-
-let green = () => {
-  console.log("This is a function expression");
-}
-
-callbackFunction(green);
+console.log(helper("Alex"));
---
+++ helper.js
@@ -0,0 +1,5 @@
+const helper = (data) => {
+  return `${data} is logged in`;
+};
+
+module.exports = helper
