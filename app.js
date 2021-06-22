3292ec3b8d6cd18acd326887865011a563c9725b
DouglasVDM <douglasvdmerwe@gmail.com>
2021-06-22 21:46:35 +0200

    writing a file
    take the contents of text variable and push it into write.txt file

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const fs = require("fs");
 
+// 1st => read this file
 const text = fs.readFileSync("read-me.txt", "utf-8");
 
-console.log(text);
+// 2nd => take the contents of text variable and push it into write,txt
+fs.writeFileSync("write.txt", text);
